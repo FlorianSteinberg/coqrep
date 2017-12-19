ba212780f92abaa1c7091c18acbe62115f077351
Florian Steinberg <steinberg@mathematik.tu-darmstadt.de>
2017-12-19 17:22:52 +0100

    - added documentation for multivalued functions.

--- functions.v
+++ functions.v
@@ -7,14 +7,34 @@ Unset Strict Implicit.
 Unset Printing Implicits Defensive.
 
 Notation "S ->> T" := (S -> T -> Prop) (format "S ->> T", at level 2).
+(*This is the notation I use for multivalued functions *)
+
+Definition mf_concat (R S T : Type) (f : R ->> S) (g : S ->> T) : R ->> T :=
+  fun r t => forall s, f r s -> g s t.
+(* Eventhough multivalued functions are relations, this is different from the relational
+composition which would read "fun r t => exists s, f r s -> g s t." *)
+
+Definition mf_sum (S S' T T' : Type) (f : S ->> T) (g : S' ->> T') : (S + S') ->> (T + T') :=
+  fun c x => match c with
+    | inl a => match x with
+      | inl y => f a y
+      | inr z => False
+    end
+    | inr b => match x with
+      | inl y => False
+      | inr z => g b z
+    end
+  end.
 
 Definition mf_prod (S S' T T' : Type) (f : S ->> T) (g : S' ->> T') : (S * S') ->> (T * T') :=
   fun c x => f c.1 x.1 /\ g c.2 x.2.
 
 Notation "f , g" := (mf_prod f g) (format "f , g", at level 50).
+(*This is the notation for the tupling of multifunctions, it clashes with the pair notation *)
 
 Definition is_sing S T (f: S ->> T) :=
   forall s t t', and (f s t) (f s t') -> t = t'.
+(* a single valued function is still a PARTIAL function *)
 
 Lemma prod_sing S S' T T' (f: S ->> T) (g : S' ->> T') :
   is_sing f /\ is_sing g -> is_sing (f , g).
@@ -39,9 +59,28 @@ Proof.
   by exists (pair c d).
 Qed.
 
+Definition dom S T (f: S ->> T) s := exists t, f s t.
+
+Definition is_total S T (f: S ->> T) := forall s, dom f s.
+
+Lemma prod_total S T S' T' (f: S ->> T) (g: S' ->> T'):
+  is_total f /\ is_total g -> is_total (f,g).
+Proof.
+  move => [istotalf istotalg] s.
+  move: (istotalf s.1) (istotalg s.2) => [t fst] [t' gst'].
+  by exists (pair t t').
+Qed.
+
 Notation "S ~> T" := (S -> nat -> option T) (format "S ~> T", at level 2).
+(* I think about this type as a type of machines: For M : S ~> T I read M s n = nothing as
+"the Machine can't say anything about the return value yet" and M s n = some t as "after n
+steps the machine considers t to be the best answer". Since no assumption about concurrency
+have been made, in general a machine will produce an infinite list of return values. *)
 
 Definition eval S T (M : S ~> T) : (S ->> T) := fun a b => exists n b, M a n = some b.
+(* if M is a machine then eval M is the function the machine computes. Since no assumptions
+about convergence or concurrency have been made, the computed multivalued function need
+neither be singlevalued nor total. *)
 
 Definition is_comp S T (f: S ->> T):=
   exists M, forall a b, f a b -> eval M a b.
