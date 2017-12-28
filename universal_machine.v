fd8b86df072108e54c9d231ba7524adbd36dc0b1
Florian Steinberg <steinberg@mathematik.tu-darmstadt.de>
2017-12-28 10:50:42 +0100

    - improvements to the formulation of the universal machine.

--- universal_machine.v
+++ universal_machine.v
@@ -2,60 +2,72 @@
 that the machine is actually universal. The universal machine is a machine of type two
 and it should work for any continuous function from B -> B. Usually B is the Baire space,
 here, i.e. the set of all mappings from strings to strings. However, since I don't want
-to rely on a handwritten type of strings as I attempted in the file "operators.v", and
-use more generaly a space S -> T as substitute for B, I have to make some assumptions
-about the types S and T. This is why I came up with "SizeTypes", that are defined in the
-separate file "SizeTypes.v" and are used here. *)
-Load size_types.
+to rely on a handwritten type of strings as I attempted in the file "operators.v" I use
+more generaly a space S -> T as substitute for B. *)
+Load functions.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicits Defensive.
 
-Fixpoint equal_on S T (phi : S -> T) (psi : S -> T) (L : list S) :=
+Fixpoint equal_on (S T : Type) (phi psi : S -> T) (L : list S) :=
   match L with
     | nil => True
     | cons s K => (phi s = psi s) /\ (equal_on phi psi K)
   end.
 Notation "phi 'and' psi 'coincide_on' L" := (equal_on phi psi L) (at level 2).
 
-Definition is_continuous (S : size_type) (S' T T' :Type) (F: (S -> T) -> S' -> T') :=
-  forall phi s', exists (L : list S),
-  forall psi, phi and psi coincide_on L ->  F phi s' = F psi s'.
-(* This notion is porbably way to inefficient to be of any use. *)
+Definition is_cont (S T S' T' : Type) (F : (S -> T) ->> (S'-> T')) :=
+      forall phi (s': S'), exists (L : list S), forall psi, phi and psi coincide_on L ->
+          forall Fphi Fpsi : S' -> T', F phi Fphi -> F psi Fpsi -> Fphi s' = Fpsi s'.
+Notation "F 'is_continuous'" := (is_cont F) (at level 2).
 
-Notation operator S T S' T' := (nat -> (S -> T) -> S' -> option T').
+Require Import FunctionalExtensionality.
+Lemma cont_to_sing (S T S' T' : Type) F: @is_cont S T S' T' F -> F is_single_valued.
+Proof.
+  move => cont phi psi psi' [psivphi psi'vphi].
+  apply functional_extensionality => a.
+  move: cont (cont phi a) => _ [L] cont.
+  have: (forall K, phi and phi coincide_on K).
+  by elim.
+  move => equal.
+  by apply: (cont phi (equal L) psi psi').
+Qed.
 
-Fixpoint U n psi a := match n with
+Definition iscont (S T S' T': Type) (F: (S-> T) -> S' -> T') :=
+  forall phi (s': S'), exists (L : list S), forall psi,
+    phi and psi coincide_on L -> F phi s' = F psi s'.
+
+Lemma continuity S T S' T' (F: (S-> T) -> S' -> T') :  iscont F <-> is_cont (F2MF F).
+Proof.
+  split.
+  - move => cont phi s'.
+    move: cont (cont phi s') => _ [L cond].
+    exists L => psi coin Fphi Fpsi iv iv'.
+    rewrite -iv -iv'.
+    by apply (cond psi).
+  - move => cont phi s'.
+    move: cont (cont phi s') => _ [L cond].
+    exists L => psi coin. 
+    by apply: (cond psi coin (F phi) (F psi)).
+Qed.
+
+Fixpoint U' (S T S' T' : Type) n (psi: S' * list T -> S + T') (phi: S -> T) (L: S' * list T) :=
+match n with
   | 0 => None
-  | S n => match proj0 psi a with
-    | es => None
-    | sO c => Some c
-    | sI c => U n psi (pair a c)
+  | S n => match psi L with
+    | inr c => Some c
+    | inl c => U' n psi phi (L.1, cons (phi c) L.2)
   end
 end.
 
-Definition psi_ F m a : option str :=
-  if numberelts a <= m then None
-    else Some(F (fun b => elt(a)(nat_of_pos(bin b)+1))(elt(a)(0))).
+Definition U (S T S' T' : Type) n (psi: S' * list T -> S + T') (phi: S -> T) a :=
+U' n psi phi (a,nil).
+(* This is what I want to prove to be a universal machine. *)
 
-Lemma psigood F: iscon F -> forall a phi,
-  exists m, psi_ F m (pair a (begseg phi m)) = Some(F phi a).
-
-Lemma universaloperator F : iscon F -> 
-  exists psiF : B, forall phi,
-    is_value U (pairing psiF phi) (F phi).
+Lemma U_is_universal S T S' T' (F:(S -> T) ->> (S' -> T')):
+  (exists s: S, True) -> F is_continuous ->
+    exists psi, forall phi Fphi, F phi Fphi -> forall a, exists n, U n psi phi a = Some (Fphi a).
 Proof.
-  move => isc.
-  move: choice => whatever.
-  set R := fun a b => exists k : nat,
-    U k (pairing psiF phi) a = Some (F phi a) /\
-    (forall n : nat, n <= k -> U n (pairing psiF phi) a = None).
-  exists (fun m => psi_ F (2^m)).
-  move => phi n.
-  move: (isc phi n).
-  case.
-  move => m ismod.
-  exists m.
-  move => b bln.
-  move: psigood => psigd.
+  move => [s _] cont.
+Admitted.
