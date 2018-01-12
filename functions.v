15380a5f2adeb60d7f5d63768c2e5ebe8dabf137
Florian Steinberg <steinberg@mathematik.tu-darmstadt.de>
2018-01-12 15:40:43 +0100

    - improvements of single valued and surjective.

--- functions.v
+++ functions.v
@@ -35,32 +35,31 @@ about them because I needed them. *)
 Notation "f \, g" := (mf_prod f g) (at level 50).
 (*This is the notation for the tupling of multifunctions*)
 
-Definition is_sing_in S T (f: S ->> T) (P: T -> Prop) :=
-  forall s t t', P t -> P t' -> (f s t) -> (f s t') -> t = t'.
-Notation "f 'is_single_valued_in' P" := (is_sing_in f P) (at level 2).
-Definition is_sing S T (f: S ->> T) := is_sing_in f (fun s=>True).
+Definition is_sing_wrt S T (f: S ->> T) (R: T -> T -> Prop) :=
+  forall s t t', (f s t) -> (f s t') -> R t t'.
+Notation "f 'is_single_valued_wrt' P" := (is_sing_wrt f P) (at level 2).
+Definition is_sing S T (f: S ->> T) := is_sing_wrt f (fun s t=> s = t).
 Notation "f 'is_single_valued'" := (is_sing f) (at level 2).
 (* a single valued function is still a partial function *)
 
-Lemma prod_sing S S' T T' (f: S ->> T) (g : S' ->> T') :
-  f is_single_valued /\ g is_single_valued -> (f \, g) is_single_valued.
+Lemma prod_sing_wrt S S' T T' (f: S ->> T) (g : S' ->> T') R R' :
+  f is_single_valued_wrt R /\ g is_single_valued_wrt R'
+  	-> (f \, g) is_single_valued_wrt (fun p q => R p.1 q.1 /\ R' p.2 q.2).
 Proof.
-  move => [Fissing Gissing] a x y _ _.
-    move => [a0isxname a1isxname] [a0isyname a1isyname].
-    apply: injective_projections.
-    - by apply: (Fissing (a.1) x.1 y.1).
-    - by apply: (Gissing (a.2) x.2 y.2).
+move => [Fsing Gsing] [a1 a2] [x1 x2] [y1 y2] [fv1 gv1] [fv2 gv2].
+split.
+	by apply (Fsing a1 x1 y1).
+by apply (Gsing a2 x2 y2).
 Qed.
 
-Lemma prod_sing_in S S' T T' (f:S ->> T) (g : S' ->> T') (P: T-> Prop) (Q: T' -> Prop) :
-  f is_single_valued_in P /\ g is_single_valued_in Q
-    -> (f \, g) is_single_valued_in (fun x => P x.1 /\ Q x.2).
+Lemma prod_sing S S' T T' (f: S ->> T) (g: S' ->> T'):
+  f is_single_valued /\ g is_single_valued
+  	-> (f \, g) is_single_valued.
 Proof.
-  move => [Fissing Gissing] a x y [px1 px2] [qx1 qx2].
-    move => [a0isxname a1isxname] [a0isyname a1isyname].
-    apply: injective_projections.
-    - by apply: (Fissing (a.1) x.1 y.1).
-    - by apply: (Gissing (a.2) x.2 y.2).
+move => [fsing gsing] [a1 a2] [x1 x2] [y1 y2] [fv1 gv1] [fv2 gv2].
+apply: injective_projections.
+	by apply (fsing a1 x1 y1).
+by apply (gsing a2 x2 y2).
 Qed.
 
 Definition range S T (f: S ->> T) (t : T) := exists s, f s t.
@@ -78,9 +77,9 @@ Proof.
   by exists (s',t').
 Qed.
 
-Definition is_sur_on S T (f: S->> T) (A: T -> Prop) :=
-  forall t,  A t <-> range f t.
-Notation "f 'is_surjective_on' A" := (is_sur_on f A) (at level 2).
+Definition is_sur_wrt S T (f: S->> T) (A: T -> Prop) :=
+  forall t,  A t -> (exists s, f s t /\ forall s t', f s t -> f s t' -> A t').
+Notation "f 'is_surjective_wrt' A" := (is_sur_wrt f A) (at level 2).
 
 Definition dom S T (f: S ->> T) s := exists t, f s t.
 Notation "s 'from_dom' f" := (dom f s) (at level 2).
@@ -102,32 +101,39 @@ Definition mf_composition (R S T : Type) (f : S ->> T) (g : R ->> S) : R ->> T :
 composition which would read "fun r t => exists s, f r s /\ g s t." *)
 Notation "f 'o' g" := (mf_composition f g) (at level 2).
 
-Lemma single_valued_composition R S T (f: S ->> T) (g : R ->> S) :
-	f is_single_valued -> g is_single_valued_in (dom f) -> f o g is_single_valued.
+Lemma single_valued_composition_wrt R S T (f: S ->> T) (g : R ->> S) :
+	f is_single_valued -> g is_single_valued_wrt (fun s s' => forall t, f s t -> f s' t) -> f o g is_single_valued.
 Proof.
-move => fsing gsing r t t' _ _ [[s] [grs fst]] prop [[s'] [grs' fs't'] ] _.
-	apply: (fsing s t t') => //.
-have: s = s'.
-	apply: (gsing r s s') => //.
-by apply (prop s).
-by apply (prop s').
-move => eq.
+move => fsing gsing r t t' [][] s [] grs fst prop [][]s' [] grs' fs't' prop'.
+move: (gsing r s s' grs grs' t fst) => fs't.
+move: (fsing s t t') => eq.
+move: (fsing s' t t') => eq''.
+rewrite eq => //.
+by rewrite -eq''.
+Qed.
+
+Lemma single_valued_composition R S T (f: S ->> T) (g : R ->> S) :
+	f is_single_valued -> g is_single_valued -> f o g is_single_valued.
+Proof.
+move => fsing gsing r t t' [][] s [] grs fst prop [][]s' [] grs' fs't' prop'.
+move: (gsing r s s' grs grs') => eq.
+move: (fsing s t t') => eq'.
+rewrite eq' => //.
 by rewrite eq.
 Qed.
 
-Lemma surjective_composition R S T (f: S ->> T) (g : R ->> S):
-	f is_surjective -> g is_surjective_on (dom f) -> f o g is_surjective.
+Lemma surjective_composition_wrt R S T (f: S ->> T) (g : R ->> S):
+	f is_surjective -> g is_surjective_wrt (dom f) -> f o g is_surjective.
 Proof.
 move => fsur gsur t.
 move: (fsur t) => [s] fst.
-have: s from_range g.
-	apply gsur.
+have: s from_dom f.
 	by exists t.
-move => [r] grs.
+move => sdomf.
+move: (gsur s sdomf) => [] r [] grs cond.
 exists r.
 split.
 	by exists s.
-move => s0 grs0.
-apply (gsur s0).
-by exists r.
+move => s'.
+by apply: (cond r s').
 Qed.
