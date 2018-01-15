a30a608484cdd03891f8ab79b5d6edc4cddc5e49
FlorianSteinberg <florian.steinberg@inria.fr>
2018-01-15 10:52:00 +0100

    - progress?

--- universal_machine.v
+++ universal_machine.v
@@ -46,17 +46,103 @@ end.
 
 Context (F: B ->> B').
 
-Definition is_fun_for (phi :B) L :=
-forall q a, List.In (q, a) L -> List.In (q, phi q) L.
-Notation "phi 'is_function_for' L" := (if_fun_for phi L) (at level 2).
+Notation L2MF L := (fun q a => List.In (q, a) L).
+
+Definition flst (phi: B) L := zip L (map phi L).
+Notation function_list phi L := (flst phi L).
+
+Lemma in_function_list phi qa:
+	forall L, List.In qa (function_list phi L) -> phi (qa.1) = qa.2.
+Proof.
+elim => // q L ih ass.
+case: ass => //; rewrite (surjective_pairing qa).
+by case => eq1 eq2 /=; rewrite -eq1.
+Qed.
+
+Lemma function_list_in phi q:
+	forall L, (List.In q L -> List.In (q, phi q) (function_list phi L)).
+Proof.
+elim => // q' L ih ass.
+case: ass => H.
+	by left; rewrite H.
+by move; right; apply: ih.
+Qed.
+
+Lemma function_in_list phi qa:
+	forall L, List.In qa (function_list phi L) -> List.In qa.1 L.
+Proof.
+elim => // a L ih [].
+	by rewrite (surjective_pairing qa); case => eq _; left.
+by move => stuff; right; apply: ih.
+Qed.
+
+Lemma choice_function_list phi L:
+	phi is_choice_for (L2MF (function_list phi L)).
+Proof.
+move => q [] a listin.
+split.
+	by exists a; apply (in_function_list listin).
+move => a' phiqa'.
+by rewrite -phiqa'; apply: (function_list_in phi (function_in_list listin)).
+Qed.
+
+Lemma coin_choice_flst phi psi:
+	forall L, phi is_choice_for (L2MF (function_list psi L))
+	<->
+	phi and psi coincide_on L.
+Proof.
+elim.
+	by split => // _ /= q [] a false; exfalso.
+move => q L.
+split.
+	move: H => [] ih _ icf.
+	split.
+		case: (icf q).
+			by exists (psi q); apply: (function_list_in psi); left.
+		move => [] a phiqa prop.
+		move: (in_function_list (prop a phiqa)) => /= psiqa.
+		by rewrite psiqa -phiqa.
+	apply ih => q' [] a inlist.
+	move: (in_function_list inlist) => /= eq.
+	split.
+		exists a; case: (icf q') => /=.
+			by exists a; right.
+		move => [] a' phiq'a' prop.
+		case: (prop a' phiq'a').
+			move: prop => _ [] eq1 eq2.
+			by rewrite -eq -{2}eq1 eq2.
+		move: prop => _ listin.
+		move: (in_function_list listin) => /= eq'.
+		by rewrite -eq eq'.
+	move: (function_in_list inlist) => /= listin a' phiq'a'.
+	case: (icf q').
+		by exists a; right.
+	move => _ prop.
+	case: (prop a' phiq'a').
+		move => [] eq1 eq2.
+		by rewrite -eq2 eq1; apply: function_list_in.
+	by move => stuff.
+move => coin q' [] a inlist.
+split.
+	by exists (phi q').
+move: coin.1 => eq a' phiq'a'.
+rewrite -phiq'a'.
+case: (function_in_list inlist) => /=.
+	move => eq'.
+	by left; rewrite -eq' eq.
+move => listin;right.
+have: List.In q' (q::L) by right.
+move => listin2.
+move: ((coin_and_list_in phi psi (q::L)).1 coin q' listin2) => eq'.
+by rewrite eq'; apply: (function_list_in psi).
+Qed.
 
 (*This should at some point go into an appropriate section: *)
 Lemma extend_list:
-		exists listf, forall (L : list (Q*A)) (q:Q) b, List.In (q, b) L ->
-    	List.In (q, (listf L) q) L.
+	exists listf, forall (L: list (Q * A)), (listf L) is_choice_for (L2MF L).
 Proof.
 set R := (fun (L : Q * list(Q * A)) (a: A) =>
-	forall b, List.In (L.1,b) L.2 -> List.In (L.1,a) L.2).
+	forall b, (L2MF L.2) L.1 b -> (L2MF L.2) L.1 a).
 have : forall L, exists b, R L b.
 	move => [q L].
 	case: (classic (exists a, List.In (q,a) L)).
@@ -64,62 +150,43 @@ have : forall L, exists b, R L b.
 		by exists a.
 	move => false.
 	exists None.
-	move => /= a inlist.
+	move => a inlist.
 	exfalso; apply: false.
 	by exists a.
 move => cond.
 move: ((@choice (Q*list(Q * A)) A R) cond) => [listf] listfprop.
 exists (fun L => (fun q => listf (q,L))).
-move => L q.
-apply: (listfprop (q, L)).
+move => L q e.
+split.
+	by exists (listf (q,L)).
+rewrite /F2MF.
+move: e => [] a inlist b v.
+move: (listfprop (q, L) a inlist) => /= asdf.
+by rewrite v in asdf.
 Qed.
 
 Lemma continuous_lists:
 	F is_continuous ->
-		exists phi', forall L: list (Q*A), ((exists phi, phi from_dom F
-			/\ forall s, List.In s (unzip1 L) -> List.In (s,phi s) L) ->
-    	(phi' L) from_dom F)
-    /\ forall s, List.In s (unzip1 L) -> List.In (s,(phi' L) s) L.
+		exists phi', forall L: list (Q*A),
+		((exists phi, phi from_dom F /\ phi is_choice_for (L2MF L)) ->
+			(phi' L) from_dom F)
+    /\ (phi' L) is_choice_for (L2MF L).
 Proof.
 move: extend_list => [] listf listfprop.
-
-set R := (fun (L : list(Q * A)) (psi:Q -> A) =>
-	((exists phi Fphi, F phi Fphi 
-		/\ forall s, List.In s (unzip1 L) -> List.In (s,phi s) L) ->
-		psi from_dom F)
-	/\ forall s, List.In s (unzip1 L) -> List.In (s,psi s) L).
+set R := (fun L (psi: B) =>
+	((exists phi, phi from_dom F /\ phi is_choice_for (L2MF L)) -> psi from_dom F)
+	/\ psi is_choice_for (L2MF L)).
 have : forall L, exists psi, R L psi.
 	move => L.
-    case: (classic (exists phi Fphi, F phi Fphi
-    	 /\ forall s, List.In s (unzip1 L) -> List.In (s,phi s) L)).
-    	move => [psi] [Fpsi] [v prop].
-      exists psi.
-      split => //.
-      by exists Fpsi.
-    move => false.
-    exists (listf L).
-    split.
-    	move => stuff.
-    	by exfalso; apply false.
-    move => s inlist.
-    have: exists a, List.In (s,a) L.
-    	move: s inlist.
-    	elim L => //.
-    	move => p K ih q' ass.
-    	case ass.
-   		exists p.2.
-   		rewrite -H.
-    	left.
-    	by rewrite -surjective_pairing.
-    	move => q'inlist.
-    	move: (ih q' q'inlist) => [] a ainlist.
-    	exists a.
-    	by right.
-    move => [] a ainlist.
-		by apply: (listfprop L s a).
-  move => cond.
-  move: ((@choice (list(Q * A)) (Q -> A) R) cond) => [phi'] phi'prop.
-  by exists phi'.
+  case: (classic (exists phi, phi from_dom F /\ phi is_choice_for (L2MF L))).
+  	move => [psi] [] psifd psic.
+    by exists psi.
+  move => false.
+  exists (listf L).
+  by split => //.
+move => cond.
+move: ((@choice (list(Q * A)) (Q -> A) R) cond) => [phi'] phi'prop.
+by exists phi'.
 Qed.
 
 Definition is_count T :=
@@ -127,15 +194,12 @@ Definition is_count T :=
 Notation "T 'is_countable'" := (is_count T) (at level 2).
 
 Lemma U_is_universal:
-	Q is_countable ->
-  (exists a: A, True) ->
-  (exists a':A', True) ->
-  F is_continuous ->
+	Q is_countable -> F is_continuous ->
   	exists psi, forall phi, (exists Fphi, F phi Fphi) ->
     forall (Fphi: Q'->A') a, exists n, U n psi phi a = Some (Fphi a).
 Proof.
-  move => [cnt sur] [t _] [t' _] cont.
-  move: sur (minimal_section sur) => _ [] sec [] issec sprop.
+  move => [cnt sur] cont.
+  move: sur (minimal_section sur) => _ [] sec [] issec smin.
   set init_seg := fun m => in_seg cnt m.
 
   set R := fun phi psi => ((exists psi', F phi psi') -> F phi psi).
@@ -145,138 +209,52 @@ Proof.
     - move => [psi prop].
       by exists psi.
     move => false.
-    by exists (fun a => t').
+    by exists (fun a => None).
   move => cond.
   move: ((@choice ((Q -> A)) (Q' -> A') R) cond) => [Ff] Fprop.
   rewrite /R /= in Fprop.
-  move: t' R cond => _ _ _.
+  move: R cond => _ _.
 
+move: (minimal_mod_function sec cont) => [mf] mprop.
+move: (continuous_lists cont) => [] phi' phi'prop.
+have: forall phi L,
+	(phi' (flst phi L)) and phi coincide_on L.
+	move => phi L.
+	apply: (coin_choice_flst (phi' (flst phi L)) phi L).1.
+	apply phi'prop.
+move => phi'prop'.
+have: forall phi q'' psi, phi and psi coincide_on (mf phi q'') -> mf (phi q'') = 
 
-	move: (minimal_mod_function sec cont) => [mf] mprop.
-  set psiF := (fun L =>
-    if
-      (leq (size sec (mf (phi' L.2) L.1)) (length L.2))
-    then
-      (inr (Ff (phi' L.2) L.1))
-    else
-      (inl (cnt (length L.2).+1))).
-  exists psiF.
-  move => phi [Fphi FphiFphi] Fphi' q'.
-  exists (size sec (mf phi q')).
-  move: Fphi'.
-
-  set QA := fun m => zip (init_seg m) (map phi (init_seg m)).
-  have:
-  	size sec (mf (phi' (QA (size sec (mf phi q')))) q')
-  	<= size sec (mf phi q').
-  apply: (mprop).2.
-  move => psi coin Fphi' Fphi'Fphi' Fpsi FpsiFpsi.
-  replace (Fpsi q') with (Fphi q').
-  apply: (mprop.1 phi q' (phi' (QA (size sec (mf phi q'))))) => //.
-  have: 
+set psiF := (fun L =>
+  if
+    (leq (size sec (mf (phi' L.2) L.1)) (length L.2))
+  then
+    (inr (Ff (phi' L.2) L.1))
+  else
+    (inl (cnt (length L.2).+1))).
+exists psiF.
+move => phi [Fphi FphiFphi] Fphi' q'.
+exists (size sec (mf phi q')).
+set QA := fun m => function_list phi (init_seg m).
+have: size sec (mf (phi' (QA (size sec (mf phi q')))) q') <= size sec (mf phi q').
+	apply mprop.
+	move => psi coin Fphi'L val Fpsi FpsiFpsi.
+	replace (Fpsi q') with (Fphi q').
+	apply: (mprop.1 (phi' (QA (size sec (mf phi q')))) q' psi) => //.
+  apply: (listf_prop 
+  (phi' (QA (size sec (mf phi q'))))
+  psi
+  (mf (phi' (QA (size sec (mf phi q')))) q')).1.
+	move: (phi'prop (function_list psi (mf (phi' (QA (size sec (mf phi q')))) q'))).2.
+	have:
   	forall m, m < size sec (mf phi q') ->
-  	step psiF phi q' (QA m) = inr (QA m.+1).
+  	U_step psiF phi q' (QA m) = inr (QA m.+1).
   elim.
   	move => ineq.
-  	rewrite /step.
-  	rewrite /psiF /QA /=.
-  have: forall m< size sec (mf phi q') ->
-  	rec m psiF phi q' = inr (zip (init_seg m.+1) (map phi (init_seg m.+1))).
-  	elim.
-  		move => ineq.
-  		rewrite /rec.
-  		rewrite /step.
-    move => ineq /=.
-    have: ( size sec (mf (phi' nil) q')) <= 0.
-   		rewrite eq.
-    	apply: (mprop.2).
-    	move => psi pep Fphi' val Fpsi FpsiFpsi.
-    	replace (Fphi' q') with (Fphi q');last first.
-    	apply: (mprop.1 (phi) q' (phi' nil) _ ) => //.
-    		move => .
-    		apply: (mprop.1 (phi' nil) q' (psi)) => //.
-    		apply: (@list_size Q A cnt sec issec (mf (phi' nil) q')).
-    		rewrite -eq //.
-    		have: F (phi' nil) Fphi.
-    	move: (mprop.1).
-    	split.
-    	have: (mf (phi' nil) q') = nil.
-    		move: (fprop (phi, s') (phi' nil)).
-    replace (Ff (phi' [::]) s') with (Fphi' s') => //.
-      have: f (phi' nil, s') = 0.
-      case: (classic (exists Fpsi, F (phi' nil) Fpsi)).
-      - move => [Fpsi] v1.
-        move: (fprop (phi, s') (phi' nil)) => stuff.
-        have: (forall m : nat, m < f (phi, s')
-        	-> (phi, s').1 (cnt m) = phi' [::] (cnt m)).
-        move => m ml0.
-        exfalso.
-        apply (PeanoNat.Nat.nlt_0_r m).
-        by rewrite zero.
-        move => ass.
-        move: stuff ass (stuff ass Fphi v) => _ _ [] [] Fpsi' v3 lala.
-        move: lala (lala Fpsi v1) => _ /= v4.
-      - done.
-      apply (fprop (phi,s') (phi' nil)).
-      rewrite zero.
-      move => m H.
-      exfalso.
-      by apply: (PeanoNat.Nat.nlt_0_r m).
-      by replace ((phi,s').1) with phi.
-      - apply: (Fprop (phi' nil)).
-        apply phiprop.
-        exists phi.
-        exists Fphi.
-        split.
-        - done.
-        done.
-    - move => n ih two.
-      have: f(phi,s') = n.+1.
-      - move: (Minus.pred_of_minus (f(phi,s')).+1) => H.
-        by rewrite -{2}two /= in H.
-      move: two => _ one.
-      rewrite /U /U' /psiF /=.
-      replace (Ff (phi' [::]) s') with (Fphi s').
-      - done.
-      apply (fprop (phi,s') (phi' nil)).
-      rewrite one.
-      move => m H /=.
-      exfalso.
-      by apply: (PeanoNat.Nat.nlt_0_r m).
-      by replace ((phi,s').1) with phi.
-      - apply: (Fprop (phi' nil)).
-        apply phiprop.
-        exists phi.
-        exists Fphi.
-        split.
-        - done.
-        done.
-      
-      exists Fphi'.
-      case: (classic ((exists (Fphi : S' -> T'),
-        F (phi' nil) Fphi /\
-        (forall (s : S) (c : T),
-         List.In (s, c) [::] -> List.In (s, (phi' nil) s) [::])))).
-      move => [Fpsi] [v3 c].
-      by exists Fpsi.
-      move => false.
-      exists (Fphi).
-      apply NNPP.
-      move => nv.
-      apply false.
-      exists (phi' nil).
-      exists Fphi.
-      split.
-      split; last first.
-      replace Fphi with Fpsi => //.
-      apply functional_extensionality.
-      move => x.
-      move: (cont (phi' nil) x) => [L] stuff.
-      apply (stuff (phi' nil)).
-      done.
-      move: (cont_to_sing cont) => sing.
-      move: (sing (phi' nil) Fphi Fpsi).
-      apply: cont.
+  		rewrite /U_step.
+  		rewrite /psiF /=.
+  		replace (QA 0) with (@nil (Q*A)).
+
 (* This is probably not true without further assumptions... also, instead of arbitrary
 certificates, the function f should probably use minimal certificates for it to work
 even in special cases. *)
