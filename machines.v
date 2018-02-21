0a098e8d200db72e3a42b3d66de3ef6d1a70b848
Florian Steinberg <steinberg@mathematik.tu-darmstadt.de>
2018-02-21 14:33:16 +0100

    added \ in front of natural language.

--- machines.v
+++ machines.v
@@ -23,11 +23,11 @@ Definition eval (N: Q ~> A) q Nq :=
 	exists n, N n q = some (Nq).
 
 Definition comp (N: Q ~> A) (f: Q ->> A) :=
-	(eval N) tightens f.
-Notation "N 'computes' f" := (comp N f) (at level 2).
+	(eval N) \tightens f.
+Notation "N '\computes' f" := (comp N f) (at level 2).
 
 Lemma comp_sing N f:
-	N computes (F2MF f) -> forall q, (eval N) q (f q).
+	N \computes (F2MF f) -> forall q, (eval N) q (f q).
 Proof.
 move => comp q.
 have ex: exists a, f q = a by exists (f q).
@@ -38,12 +38,12 @@ by exists n.
 Qed.
 
 Definition is_comp (f: Q ->> A) :=
-	{N | N computes f}.
-Notation "f 'is_computable'" := (is_comp f) (at level 2).
+	{N | N \computes f}.
+Notation "f '\is_computable'" := (is_comp f) (at level 2).
 
 Definition is_prim_rec (f: Q ->> A) :=
-	{M | M is_choice_for f}.
-Notation "f 'is_primitive_recursive'" := (is_prim_rec f) (at level 2).
+	{M | M \is_choice_for f}.
+Notation "f '\is_primitive_recursive'" := (is_prim_rec f) (at level 2).
 
 Lemma prim_rec_sing f :
 	is_prim_rec (F2MF f).
@@ -52,15 +52,12 @@ by exists f.
 Qed.
 
 Lemma prim_rec_is_comp (f: Q ->> A):
-	f is_primitive_recursive -> f is_computable.
+	f \is_primitive_recursive -> f \is_computable.
 Proof.
 move => [] M Mprop.
-exists (fun n q => Some (M q)).
-move => q ex.
+exists (fun n q => Some (M q)) => q ex.
 specialize (Mprop q ex).
-split.
-	exists (M q).
-	by exists 0.
+split; first by exists (M q); exists 0.
 move => t' ev.
 apply/ (Mprop.2 t').
 move: (ev) => [] n prop.
@@ -71,27 +68,24 @@ Definition evaltt (M: B ~>> B') phi Mphi :=
 	forall q', exists n, M n phi q' = some (Mphi q').
 
 Definition comptt (M: B ~>> B') (F: B ->> B'):=
-  (evaltt M) tightens F.
-Notation "M 'type_two_computes' F" := (comptt M F) (at level 2).
+  (evaltt M) \tightens F.
+Notation "M '\type_two_computes' F" := (comptt M F) (at level 2).
 
 Definition is_comptt (F: B ->> B') :=
-	{M| M type_two_computes F}.
-Notation "F 'is_type_two_computable'" := (is_comptt F) (at level 2).
+	{M| M \type_two_computes F}.
+Notation "F '\is_type_two_computable'" := (is_comptt F) (at level 2).
 
 Definition is_prim_rectt (F: B ->> B') :=
-	{M | M is_choice_for F}.
-Notation "F 'is_type_two_primitive_recursive'" := (is_prim_rectt F) (at level 2).
+	{M | M \is_choice_for F}.
+Notation "F '\is_type_two_primitive_recursive'" := (is_prim_rectt F) (at level 2).
 
 Lemma prim_rec_is_comp_tt (F: B ->> B'):
-	F is_type_two_primitive_recursive -> F is_type_two_computable.
+	F \is_type_two_primitive_recursive -> F \is_type_two_computable.
 Proof.
 move => [] M Mprop.
-exists (fun n phi q => Some (M phi q)).
-move => phi ex.
+exists (fun n phi q => Some (M phi q)) => phi ex.
 specialize (Mprop phi ex).
-split.
- exists (M phi) => q'.
- by exists 0.
+split; first by exists (M phi) => q'; exists 0.
 move => t' ev.
 apply/ (Mprop.2 t').
 apply functional_extensionality => q'.
@@ -100,7 +94,7 @@ by apply Some_inj.
 Qed.
 
 End MACHINES.
-Notation "f 'is_computable'" := (is_comp f) (at level 2).
+Notation "f '\is_computable'" := (is_comp f) (at level 2).
 Notation "Q ~> A" := (nat -> Q -> option A) (at level 2).
-Notation "N 'computes' f" := (comp N f) (at level 2).
-Notation "M 'type_two_computes' F" := (comptt M F) (at level 2).
+Notation "N '\computes' f" := (comp N f) (at level 2).
+Notation "M '\type_two_computes' F" := (comptt M F) (at level 2).
