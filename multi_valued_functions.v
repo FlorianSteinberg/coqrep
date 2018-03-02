a10dc2e87407aa7b95644fbca6d01afdd043e471
Florian Steinberg <steinberg@mathematik.tu-darmstadt.de>
2018-03-02 01:26:46 +0100

    improved documentation and proofs.

--- multi_valued_functions.v
+++ multi_valued_functions.v
@@ -1,5 +1,4 @@
 (* This file contains basic definitions and Lemmas about multi-valued functions *)
-
 From mathcomp Require Import all_ssreflect.
 Require Import ClassicalChoice.
 
@@ -15,6 +14,9 @@ a function should be understood as the set of all elements t such that f s t is 
 
 Definition F2MF S T (f : S -> T) s t := f s = t.
 (* I'd like this to be a Coercion but it won't allow me to do so. *)
+
+(* The following should be considered to define equality as multivalued functions *)
+Notation "f =~= g" := (forall s t, f s t <-> g s t) (at level 70).
 
 (* The domain of a multifunctions is the set of all inputs such that the value set
 is not empty. *)
@@ -43,66 +45,31 @@ with multivalued functions as morphisms *)
 Lemma mf_comp_assoc (f: S' ->> T') g (h: S ->> T) r q:
 	(f o g) o h r q <-> f o (g o h) r q.
 Proof.
-split.
-	move => [] [] s [] hrs [] [] t [] gst ftq prop cond.
-	split.
-		exists t.
-		split => //.
-		split.
-			exists s.
-			by split.
-		move => s' hrs'.
-		move: cond (cond s' hrs') =>
-			_ [] q' [] [] t' [] gs't' ft'q' cond.
-		by exists t'.
-	move => t' [] [] s' [] hrs' gs't' cond'.
-	move: (cond s' hrs') => [] q' [] [] t'' [] gs't'' ft''q'
-		cond''.
-	by apply (cond'' t').
-move => [] [] t [] [] [] s [] hrt gst prop ftq cond.
-split.
-	exists s.
-	split => //.
-	split.
-		exists t.
-		by split.
-	move => t' gst'.
-	have ghrs: g o h r t'.
-		split.
-			exists s.
-			by split.
-		move => s' hrs'.
-		by apply: (prop s' hrs').
-	by apply (cond t' ghrs).
-move => s' hrs'.
-move: (prop s' hrs') => [] t' gs't'.
-have ghrt': g o h r t'.
-	split.
-		exists s'.
-		by split.
-	move => s'' hrs''.
-	by apply (prop s'' hrs'').
-move: (cond t' ghrt') => [] q' ft'q'.
-exists q'.
-split.
-	exists t'.
-	by split.
-move => t'' gs't''.
-have ghrt'': g o h r t''.
-	split.
-		exists s'.
-		by split.
-	move => s'' hrs''.
-	by apply (prop s'').
-by apply (cond t'' ghrt'').
+split => [ [] [] s [] hrs [] [] t []| [] [] t [] [] [] s [] ].
+	split => [ | t' [] [] s' [] hrs'].
+		exists t;	split => //.
+		split => [ | s' hrs']; first by exists s.
+		by move: (b1 s' hrs') => [] x [] [] t' []; exists t'.
+	by move: (b1 s' hrs') => [] q' comp gs't _; apply comp.2.
+split => [ | s' hrs'].
+	exists s; split => //.
+	split => [ | t' gst']; first by exists t.
+	suffices ghrs: g o h r t' by apply (b2 t' ghrs).
+	by split => [ | s' hrs']; [exists s | apply b0].
+move: (b0 s' hrs') => [] t' gs't'.
+have ghrt': g o h r t'
+	by split => [ | s'' hrs'']; [exists s' | apply b0].
+move: (b2 t' ghrt') => [] q' ft'q'; exists q'.
+split => [ | t'' gs't'']; first by exists t'.
+suffices ghrt'': g o h r t'' by apply b2.
+by split => [ | s'' hrs'']; [exists s' | apply b0].
 Qed.
 
-(* The following should be considered to define equality as multivalued functions *)
-Notation "f =~= g" := (forall s t, f s t <-> g s t) (at level 70).
-
+(* the following should be called "is_mono" and "is_epi", but we consider the multi
+valued functions to be functions and thus call the notions surjective and injective.
+This will be further justified below when choice functions are introduced. *)
 Definition is_sur (f: S ->> T):= forall Q (g h: T ->> Q), g o f =~= h o f -> g =~= h.
 Notation "f '\is_surjective'" := (is_sur f) (at level 2).
-
 Definition is_inj (f: S ->> T):= forall Q (g h: Q ->> S), f o g =~= f o h -> g =~= h.
 Notation "f '\is_injective'" := (is_inj f) (at level 2).
 
@@ -129,9 +96,7 @@ Notation "f '\is_single_valued'" := (is_sing f) (at level 2).
 
 Lemma fun_to_sing (f: S-> T):
 	(F2MF f) \is_single_valued.
-Proof.
-by move => s t t' H H0; rewrite -H0.
-Qed.
+Proof. by move => s t t' H H0; rewrite -H0. Qed.
 
 Definition codom S T (f: S ->> T) (t : T) := exists s, f s t.
 Notation "t '\from_codom' f" := (codom f t) (at level 2).
@@ -149,47 +114,29 @@ move => fsur t.
 pose g t' b := t = t' /\ b = true.
 pose h t' b := t = t' /\ b = false.
 apply NNPP => notcodom.
-suffices: g =~= h.
-	move => eq.
-	move: (eq t true) => ha.
-	case: (classic (g t true)) => ass.
-		by move: (ha.1 ass) => [] _ H.
-	by apply ass.
-apply (fsur bool g h).
-move => s b.
-by split => [] [] [] t' [] val1 val2 prop; by exfalso; apply notcodom; exists s; rewrite val2.1.
+suffices eq: g =~= h.
+	case: (classic (g t true)) => ass; last by apply ass.
+	by case: ((eq t true).1 ass).
+apply (fsur bool g h) => s b.
+split => [] [] [] t' [] val1 val2 prop;
+	by exfalso; apply notcodom; exists s; rewrite val2.1.
 Qed.
 
 (* The opposite implication does not hold in general but for single valued functions it is true. *)
 Lemma sing_cotot_sur f:
 f \is_single_valued -> (f \is_cototal <-> f \is_surjective).
 Proof.
-move => fsing.
-split;last first.
-	exact: sur_cotot.
-move => fcotot Q g h eq t q.
-split => ass.
-	move: (fcotot t) => [] s fst.
-	have gfsq: (g o f s q).
-		split.
-			by exists t.
-		move => t' fst'.
-		exists q.
-		by rewrite (fsing s t' t).
-	move: ((eq s q).1 gfsq) => [] [] t' [] fst' ht'q prop.
-	by rewrite (fsing s t t').
-move: (fcotot t) => [] s fst.
+split => [fcotot Q g h eq t q| ]; last exact: sur_cotot.
+split => ass; move: (fcotot t) => [] s fst.
+	suffices gfsq: (g o f s q).
+		by move: ((eq s q).1 gfsq) => [] [] t' [] fst'; rewrite (H s t t').
+	by split => [ | t' fst']; [exists t | exists q; rewrite (H s t' t)].
 have hfsq: (h o f s q).
-	split.
-		by exists t.
-	move => t' fst'.
-	exists q.
-	by rewrite (fsing s t' t).
-move: ((eq s q).2 hfsq) => [] [] t' [] fst' ht'q prop.
-by rewrite (fsing s t t').
+	by split => [ | t' fst']; [ exists t| exists q; rewrite (H s t' t) ].
+by move: ((eq s q).2 hfsq) => [] [] t' [] fst'; rewrite (H s t t').
 Qed.
 
-(* Thus, there is a very simple property that classifies being a partial surjective function.*)
+(* Due to the above, the following is named correctly.*)
 Definition sur_par_fun S T (f: S ->> T) :=
   f \is_single_valued /\ f \is_cototal.
 Notation "f '\is_surjective_partial_function'" := (sur_par_fun f) (at level 2).
@@ -200,72 +147,52 @@ Definition mf_prod S T S' T' (f : S ->> T) (g : S' ->> T') :=
 Notation "f \, g" := (mf_prod f g) (at level 50).
 (*This is the notation for the tupling of multifunctions *)
 
-Lemma prod_comp R R'
-(f: S ->> T) (g: S' ->> T') (f': R ->> S) (g': R' ->> S'):
-	forall x fx, (f \, g) o (f' \, g') x fx <-> (f o f' \, g o g') x fx.
+Lemma prod_comp R R' (f: S ->> T) (g: S' ->> T') (f': R ->> S) (g': R' ->> S'):
+	(f \, g) o (f' \, g') =~= (f o f' \, g o g').
 Proof.
-move => x ffggx.
-split.
-	move => [] [] fgx [] [] fxfgx gxfgx [] ffgxffggx gfgxffggx prop.
-	split.
-		split.
-			by exists fgx.1.
-		move => s f'xs.
-		have temp: ((s, fgx.2) \from_dom (f \, g)) by apply: ((prop (s, fgx.2))).
-		move: temp => [] [] x1 x2 [] /= fsx1.
-		by exists x1.
-	split.
-		by exists fgx.2.
-	move => s f'xs.
-	have temp: ((fgx.1,s) \from_dom (f \, g)) by apply: ((prop (fgx.1, s))).
-	move: temp => [] [] x1 x2 [] /= fsx1.
-	by exists x2.
-move => [] [] [] s1 [] fxs1 fs1ffggx prop [] [] s2 [] fxs2 fs2ffggx prop'.
-split.
-	by exists (s1,s2).
-move => [] s'1 s'2 [] fs' gs'.
-move: (prop s'1 fs') (prop' s'2 gs') => [] t fst [] t' fst'.
-by exists (t,t').
+split => [[] [] fgx [] [] | [] [] [] s1 []]; last first.
+	move => fxs1 fs1ffggx H [] [] s2 [] fxs2 fs2ffggx H'.
+	split => [ | [] s'1 s'2 [] fs' gs']; first by exists (s1,s2).
+	by move: (H s'1 fs') (H' s'2 gs') => [] t' fst [] t'' ; exists (t',t'').
+move => fxfgx gxfgx [] ffgxffggx gfgxffggx H.
+split; split => [ | s' f'xs]; [by exists fgx.1 | | by exists fgx.2 | ].
+	have temp: ((s', fgx.2) \from_dom (f \, g)) by apply: ((H (s', fgx.2))).
+	by move: temp => [] [] x1 x2 [] /= fsx1; exists x1.
+have temp: ((fgx.1,s') \from_dom (f \, g)) by apply: ((H (fgx.1, s'))).
+by move: temp => [] [] x1 x2 [] /= fsx1; exists x2.
 Qed.
 
 Lemma prod_sing (f: S ->> T) (g: S' ->> T'):
   f \is_single_valued /\ g \is_single_valued -> (f \, g) \is_single_valued.
 Proof.
 move => [fsing gsing] s t t' [] fst gst [] fst' gst'.
-apply: injective_projections.
-	by apply (fsing s.1).
-by apply (gsing s.2).
+by apply: injective_projections; [apply (fsing s.1)| apply (gsing s.2)].
 Qed.
 
-Lemma prod_dom (f: S ->> T) (g: S' ->> T'):
-  forall s t, (s, t) \from_dom (f \, g) <-> s \from_dom f /\ t \from_dom g.
+Lemma prod_dom (f: S ->> T) (g: S' ->> T') s t:
+  (s, t) \from_dom (f \, g) <-> s \from_dom f /\ t \from_dom g.
 Proof.
-split.
-	by move => [] x [] /= fsx gty; split; [exists x.1| exists x.2].
-by move => [[s' fs's] [t' ft't]]; exists (s',t').
+split; last by move => [[s' fs's] [t' ft't]]; exists (s',t').
+by move => [] x [] /= fsx gty; split; [exists x.1| exists x.2].
 Qed.
 
 Lemma prod_tot (f: S ->> T) (g: S' ->> T'):
 	f \is_total /\ g \is_total -> (f \, g) \is_total.
 Proof.
-move => [] ftot gtot [] t t'.
-apply (prod_dom f g t t').2.
+move => [ftot gtot] [t t']; apply (prod_dom f g t t').2.
 by split; [apply: ftot t| apply: gtot t'].
 Qed.
 
 Lemma tot_prod (f: S ->> T) (g: S' ->> T'):
 	S -> S' -> (f \, g) \is_total -> f \is_total /\ g \is_total.
 Proof.
-split => s.
-	by apply: ((prod_dom f g s X0).1 (H (s, X0))).1.
+split => s; first by apply: ((prod_dom f g s X0).1 (H (s, X0))).1.
 by apply: ((prod_dom f g X s).1 (H (X, s))).2.
 Qed.
 
 Lemma prod_codom (f: S ->> T) (g : S' ->> T') :
   forall s t, s \from_codom f /\ t \from_codom g -> (s,t) \from_codom (f \, g).
-Proof.
-by move => s t [[s' fs's] [t' ft't]]; exists (s',t').
-Qed.
+Proof. by move => s t [[s' fs's] [t' ft't]]; exists (s',t'). Qed.
 
 Definition tight S T (f: S ->> T) (g: S ->> T) :=
 	forall s, s \from_dom f -> s \from_dom g /\ forall t, g s t -> f s t.
@@ -279,52 +206,39 @@ if "forall s, (exists t, f(s) = t) -> g(s) = f(s)". This formula can also be wri
 "forall s t, f(s) = t -> g(s) = t" and the equivalence is proven in the next lemmas.*)
 Notation "g '\extends' f" := (forall s t, f s t -> g s t) (at level 2).
 
+(* tight is an equivalence relation *)
 Lemma tight_ref (f: S ->> T):
 	f \tightens f.
-Proof.
-done.
-Qed.
+Proof. done. Qed.
 
 Lemma tight_trans (f g h: S ->> T):
 	f \tightens g -> g \tightens h -> f \tightens h.
 Proof.
-move => ftg gth s eh.
-split.
-	apply: (ftg s (gth s eh).1).1.
-move => t fst.
-apply: ((gth s eh).2 t).
-by apply: ((ftg s (gth s eh).1).2 t).
+split => [ | t fst]; first by apply: (H s (H0 s H1).1).1.
+by apply: ((H0 s H1).2 t); apply: ((H s (H0 s H1).1).2 t).
 Qed.
 
 Lemma tightening_of_single_valued (f: S ->> T) g:
 	f \is_single_valued -> g \tightens f -> g \extends f.
 Proof.
 move => fsing gef s t fst.
-move: (gef s) => [].
-	by exists t.
-move => [] t' gst' cond.
-rewrite (fsing s t t') => //.
-by apply (cond t').
+case: (gef s); first by exists t.
+case => t' gst' cond.
+by rewrite (fsing s t t'); [ | | apply (cond t') ].
 Qed.
 
 Lemma single_valued_tightening (f: S ->> T) g:
 	g \is_single_valued -> g \extends f -> g \tightens f.
 Proof.
 move => gsing gef s [] t fst.
-split.
-	exists t.
-	by apply: (gef s t).
-move => t' gst'.
-rewrite -(gsing s t t') => //.
-by apply gef.
+split => [ | t' gst']; first by exists t; apply: (gef s t).
+by rewrite -(gsing s t t'); [ | apply gef | ].
 Qed.
 
 Lemma extension_and_tightening (f: S ->> T) g:
 	f \is_single_valued -> g \is_single_valued -> (g \extends f <-> g \tightens f).
 Proof.
-split.
-	exact: single_valued_tightening.
-exact: tightening_of_single_valued.
+split; [exact: single_valued_tightening | exact: tightening_of_single_valued] .
 Qed.
 
 (* To extend to tightenings for multivalued functions makes sense: for instance a choice
@@ -338,45 +252,30 @@ makes the above more convenient. *)
 Lemma icf_tight (g: S -> T) f:
 	g \is_choice_for f <-> (F2MF g) \tightens f.
 Proof.
-split.
-	move => icf s [] t fst.
-	split.
-		by exists (g s).
-	move => gs eq.
-	rewrite -eq.
-	by apply: (icf s t).
-move => tight s t fst.
+split => [ icf s [] t fst | tight s t fst].
+	by split => [ | gs eq ]; [exists (g s) | rewrite -eq; apply: (icf s t)].
 have ex: s \from_dom f by exists t.
-have [_ prop]:= (tight s ex).
-by apply (prop (g s)).
+by apply ((tight s ex).2 (g s)).
 Qed.
 
-Lemma exists_choice (f: S ->> T):
-	T -> exists F, F \is_choice_for f.
+Lemma exists_choice (f: S ->> T) (t: T):
+	exists F, F \is_choice_for f.
 Proof.
-move => t.
 set R := fun s t => forall t', f s t' -> f s t.
-have cond: forall s, exists t, R s t.
-	move => s.
-	case: (classic (s \from_dom f)).
-		by move => [] t' fst; exists t'.
-	move => false.
-	exists t => t' fst'.
-	by exfalso; apply false; exists t'.
-move: (choice R cond) => [] F prop.
-rewrite /R in prop;move: R cond => _ _.
-by exists F.
+suffices Rtot: R \is_total by move: (choice R Rtot) => [] F prop; exists F.
+move => s.
+case: (classic (s \from_dom f)) => [[] t' fst | false]; first by exists t'.
+by exists t => t' fst'; exfalso; apply false; exists t'.
 Qed.
 
 Lemma sing_comp (f: T ->> T') (g : S ->> T) :
 	f \is_single_valued -> g \is_single_valued -> f o g \is_single_valued.
 Proof.
-move => fsing gsing.
-move => r t t' [][] s [] grs fst prop [][] s' [] grs' fs't' prop'.
-move: (gsing r s s' grs grs') (fsing s t t') => eq eq'.
-by rewrite eq' => //; rewrite eq.
+move => fsing gsing r t t' [[] s [] grs fst _ [][] s' [] grs' fs't' _].
+by rewrite (fsing s t t') => //; rewrite (gsing r s s').
 Qed.
 End MULTIVALUED_FUNCTIONS.
+Notation "f =~= g" := (forall s t, f s t <-> g s t) (at level 70).
 Notation "S ->> T" := (S -> T -> Prop) (format "S ->> T", at level 2).
 Notation "f \, g" := (mf_prod f g) (at level 50).
 Notation "f '\is_single_valued'" := (is_sing f) (at level 2).
