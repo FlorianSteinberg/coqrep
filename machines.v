b5dbaac59dd6b45a6ab11c3de412c3ffbf313354
Florian Steinberg <steinberg@mathematik.tu-darmstadt.de>
2018-03-05 12:49:43 +0100

    added monotonicity definition.

--- machines.v
+++ machines.v
@@ -1,9 +1,5 @@
-(* This file is supposed to contain the definition of a universal machine and the proof
-that the machine is actually universal. The universal machine is a machine of type two
-and it should work for any continuous function from B -> B. Usually B is the Baire space,
-here, i.e. the set of all mappings from strings to strings. However, since I don't want
-to rely on a handwritten type of strings as I attempted in the file "operators.v" I use
-more generaly a space S -> T as substitute for B. *)
+(* This file contains some definitions of what it can mean for functions
+between different spaces to be computable. *)
 From mathcomp Require Import all_ssreflect.
 Require Import multi_valued_functions continuity.
 Require Import FunctionalExtensionality ClassicalChoice.
@@ -13,11 +9,12 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 Section MACHINES.
-Context (Q A Q' A' : Type).
+Context (Q A Q' A' C : Type).
+
 Notation B := (Q -> A).
 Notation B' := (Q' -> A').
 Notation "Q ~> A" := (nat -> Q -> option A) (at level 2).
-Notation "B ~>> B'" := (nat -> B -> Q' -> option A') (at level 2).
+Notation "B ~~> B'" := (nat -> B -> Q' -> option A') (at level 2).
 
 Definition eval (N: Q ~> A) q Nq :=
 	exists n, N n q = some (Nq).
@@ -31,10 +28,8 @@ Lemma compu_sing N f:
 Proof.
 move => comp q.
 have ex: exists a, f q = a by exists (f q).
-move: (comp q ex) => [] [] a [] n Nnqa prop.
-exists n.
-rewrite Nnqa (prop a) //.
-by exists n.
+have [[a [n Nnqa] prop]]:= (comp q ex).
+by exists n; rewrite Nnqa (prop a) //; exists n.
 Qed.
 
 Definition is_comp (f: Q ->> A) :=
@@ -47,28 +42,26 @@ Notation "f '\is_primitive_recursive'" := (is_prim_rec f) (at level 2).
 
 Lemma prim_rec_sing f :
 	is_prim_rec (F2MF f).
-Proof.
-by exists f.
-Qed.
+Proof. by exists f. Qed.
 
 Lemma prim_rec_is_comp (f: Q ->> A):
 	f \is_primitive_recursive -> f \is_computable.
 Proof.
-move => [] M Mprop.
+move => [M Mprop].
 exists (fun n q => Some (M q)) => q ex.
 move: ((icf_F2MF_tight M f).1 Mprop) => thight.
-specialize (thight q ex).
-split; first by exists (M q); exists 0.
-move => t' ev.
-apply thight.
-move: (ev) => [] n prop.
+split => [ | t' [n nprop]]; first by exists (M q); exists 0.
+apply (thight q ex).
 by apply Some_inj.
 Qed.
 
-Definition evaltt (M: B ~>> B') phi Mphi :=
+Definition monotone (M: Q ~> A):=
+	forall n m q a, n <= m -> M n q = Some a -> M m q = Some a.
+
+Definition evaltt (M: B ~~> B') phi Mphi :=
 	forall q', exists n, M n phi q' = some (Mphi q').
 
-Definition comptt (M: B ~>> B') (F: B ->> B'):=
+Definition comptt (M: B ~~> B') (F: B ->> B'):=
   (evaltt M) \tightens F.
 Notation "M '\type_two_computes' F" := (comptt M F) (at level 2).
 
@@ -94,6 +87,9 @@ apply functional_extensionality => q'.
 move: (ev q') => [] n prop.
 by apply Some_inj.
 Qed.
+
+Definition monotonett (M: B ~~> B):=
+	forall n m phi q' a', n <= m -> M n phi q' = Some a' -> M m phi q' = Some a'.
 
 End MACHINES.
 Notation "f '\is_computable'" := (is_comp f) (at level 2).
