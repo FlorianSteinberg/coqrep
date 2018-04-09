2ffaa152584363d40d17ab4bbea91a81198b5b1c
FlorianSteinberg <florian.steinberg@inria.fr>
2018-04-09 11:37:33 +0200

    made statement slightly more uniform. Proved that the target space being spread implies computable functions being prec. simplified Proof of statement about elements.

--- rs_sprd.v
+++ rs_sprd.v
@@ -1,20 +1,20 @@
 From mathcomp Require Import all_ssreflect.
 Require Import all_rs_base.
-Require Import ClassicalChoice.
+Require Import ClassicalChoice Psatz.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 Section SPREADS.
-Definition is_sprd (X: rep_space) := forall (x: X) (M: nat -> questions X -> option (answers X)),
-	(exists phi, (meval M) \tightens (F2MF phi) /\ phi \is_name_of x) -> x \is_computable_element.
-Notation "X '\is_spread'" := (is_sprd X) (at level 2).
+Definition sprd (X: rep_space) := forall (M: nat -> _ -> _),
+	{psi | forall phi (x: X), M \computes (F2MF phi) -> phi \is_name_of x -> psi \is_name_of x}.
+Notation "X '\is_spread'" := (sprd X) (at level 2).
 
 Lemma prod_sprd (X Y: rep_space):
 	X \is_spread -> Y \is_spread -> (rep_space_prod X Y) \is_spread.
 Proof.
-move => sprdx sprdy [x y] MN prop.
+move => sprdx sprdy MN.
 pose M n q := match MN n (inl q) with
 	| some a => Some a.1
 	| None => None
@@ -23,163 +23,97 @@ pose N n q := match MN n (inr q) with
 	| Some a => Some a.2
 	| None => None
 end.
-have ex: exists phi, (meval M) \tightens (F2MF phi) /\ phi \is_name_of x.
-	have [phipsi [comp [/=phinx psiny]]]:= prop.
-	exists (lprj phipsi).
-	split; last by apply phinx.
-	move => q _.
-	have qfd': (inl q) \from_dom (F2MF phipsi) by exists (phipsi (inl q)).
-	split.
-		have [a [n MNqa]]:= (comp (inl q) qfd').1.
-		by exists a.1; exists n; rewrite /M MNqa.
-	move => a [n Mqa]; rewrite /F2MF/lprj.
-	rewrite /M in Mqa.
-	have [a' [MNqa' eq]]: exists a', MN n (inl q) = some a' /\ a'.1 = a.
-		by case: (MN n (inl q)) Mqa => // a' eq; exists a'; split => //; apply Some_inj.
-	have val: (meval MN (inl q) a') by exists n.
-	have:= ((comp (inl q) qfd').2 a' val).
-	by rewrite /F2MF -eq => ->.
-have:= sprdx x M ex.
-have ex': exists psi, (meval N) \tightens (F2MF psi) /\ psi \is_name_of y.
-	have [phipsi [comp [/=phinx psiny]]]:= prop.
-	exists (rprj phipsi).
-	split; last by apply psiny.
-	move => q _.
-	have qfd': (inr q) \from_dom (F2MF phipsi) by exists (phipsi (inr q)).
-	split.
-		have [a [n MNqa]]:= (comp (inr q) qfd').1.
-		by exists a.2; exists n; rewrite /N MNqa.
-	move => a [n Mqa]; rewrite /F2MF/rprj.
-	rewrite /N in Mqa.
-	have [a' [MNqa' eq]]: exists a', MN n (inr q) = some a' /\ a'.2 = a.
-		by case: (MN n (inr q)) Mqa => // a' eq; exists a'; split => //; apply Some_inj.
-	have val: (meval MN (inr q) a') by exists n.
-	have:= ((comp (inr q) qfd').2 a' val).
-	by rewrite /F2MF -eq => ->.
-have:= sprdy y N ex'.
-move => [psi psiny] [phi phinx].
-by exists (name_pair phi psi).
+have [lpsi lpsiprop]:= sprdx M.
+have [rpsi rpsiprop]:= sprdy N.
+exists (name_pair lpsi rpsi) => [phi [x y] tight [/=phinx phiny]] .
+split; rewrite lprj_pair rprj_pair/=;
+	[apply (lpsiprop (lprj phi)) | apply (rpsiprop (rprj phi))] => // q _ .
+	have lqfd: (inl q) \from_dom (F2MF phi) by apply F2MF_tot.
+	have [[[la ra] [/=n evl]] prp]:= tight (inl q) lqfd.
+	split; first by exists la; exists n; rewrite /M evl.
+	move => a [/=m val]; rewrite /M in val.
+	case E: (MN m (inl q)) => [a' | ]; last by rewrite E in val.
+	rewrite E in val; have ev: meval MN (inl q) a' by exists m.
+	by rewrite /F2MF/lprj; have ->:= (prp a' ev); apply Some_inj.
+have lqfd: (inr q) \from_dom (F2MF phi) by apply F2MF_tot.
+have [[[la ra] [/=n evl]] prp]:= tight (inr q) lqfd.
+split; first by exists ra; exists n; rewrite /N evl.
+move => a [/=m val]; rewrite /N in val.
+case E: (MN m (inr q)) => [a' | ]; last by rewrite E in val.
+rewrite E in val; have ev: meval MN (inr q) a' by exists m.
+by rewrite /F2MF/rprj; have ->:= (prp a' ev); apply Some_inj.
 Qed.
 
-(*
-Lemma fun_sprd (X Y: rep_space) (someq: questions X): (X c-> Y) \is_spread.
+Lemma sprd_cmpt_prec (X Y: rep_space) (f: X ->> Y):
+	Y \is_spread -> f \is_monotone_computable -> f \is_prec.
 Proof.
-move => f psif prop.
-pose psifacc := fix psif' n Lq := match Lq.1 with
-	| nil => (nil, psif n Lq)
-	| cons p K => match psif n (K, Lq.2) with
-		| Some a => ((cons p K), psif n (K, Lq.2))
-		| None => (K, psif n (K, Lq.2))
-	end
-end.
-pose psif' := (fun Lq => match (psifacc (length Lq.1) Lq).2 with
-	| Some a => a
-	| None => inl someq
-end).
-suffices psif'prop: forall psi, (meval psif) \tightens (F2MF psi) -> eval (U psi) =~= eval (U psif').
-	exists psif';	rewrite /delta/=/is_fun_name/=.
-	by have [psi [tight psinf]]:= prop; rewrite -(psif'prop psi tight).
-move => psi cmpt phi Fphi.
-split => ass q'; have [n evl]:= ass q'; last first.
-	exists n.
-	have UFphiq: U_rec n psif' phi q' = inl (Fphi q').
-		by rewrite /U in evl; case: (U_rec n psif' phi q') evl => // a [<-].
-	move: evl => _.
-	have cond: exists m, exists K,
-		U_rec 0 psif' phi q' = inl (Fphi q')
-		\/
-		U_rec m psif' phi q' = inr K /\ U_step psif' phi q' K = inl (Fphi q').
-	elim: n UFphiq.
-		 move => U0; exists 0; exists nil; left.
-		move => U0 exists
-	move => n ih cond.
-	case E: (U_rec n psif' phi q') => [a | L].
-		suffices eq: a = (Fphi q').
-		rewrite eq in E; exact: ih E.
-		apply Some_inj.
-		have <-: U psif' n.+1 phi q' = Some (Fphi q') by rewrite /U cond.
-		have ineq: (n <= n.+1)%coq_nat by lia.
-		apply esym; apply/ U_mon; first apply ineq.
-		by rewrite /U E.
-	exists n; exists L.
-	split => //.
-	by rewrite /= E in cond.
-
-	rewrite /U_rec in Ua'.
-	rewrite /U_step in Ua'.
-	have: exists L, 
-	rewrite /U_rec.
-	rewrite /psif'.
-	rewrite /U.
-	have [cnt sur] := countable_questions X.
-	have fd: ((flst phi (initial_segments.in_seg cnt n), q') \from_dom (F2MF psi)).
-		exact: F2MF_tot psi ((flst phi (initial_segments.in_seg cnt n), q')).
-	have [[a [k val]]]:= cmpt ((universal_machine.flst phi (initial_segments.in_seg cnt n), q')) fd.
-	exists (max k n).
-	admit.
-admit.
-Admitted.
-*)
+move => sprd [M [Mmon Mprop]].
+exists (fun phi => projT1 (sprd (fun n:nat => M n phi))).
+apply F2MF_rlzr => phi x phinx /= [y fxy].
+have phifd: phi \from_dom (f o (rep X)).
+	exists y;	split; first by exists x.
+	by move => x' phinx'; rewrite (rep_sing X phi x' x); first by exists y.
+have [[y' [[Mphi [MphiMphi Mphiny']]]]phifd' prp]:= Mprop phi phifd.
+have phiv: (rep Y) o (eval M) phi y'.
+	split; first by exists Mphi.
+	move => psi evl; by apply phifd'.
+have [[x' [phinx' fx'y']]cnd]:= prp y' phiv.
+rewrite (rep_sing X phi x' x) in fx'y' => //.
+exists y'; split => //.
+apply/ (projT2 (sprd (fun n => M n phi)) Mphi y') => //.
+move => q _.
+have [n evl]:= MphiMphi q.
+split; first by exists (Mphi q); exists n.
+move => a [/= m eq].
+apply Some_inj; rewrite /F2MF.
+case E: (m <= n).
+	rewrite -evl.
+	apply/ Mmon; last exact eq.
+	by apply/ leP; rewrite /pickle/=E.
+rewrite -eq; apply esym.
+apply/ Mmon; last exact evl.
+rewrite /pickle/=.
+move /leP: E; lia.
+Qed.
 
 Lemma cmpt_fun_cmpt_elt (X Y: rep_space) (f: X ->> Y) (x: X) (y: Y):
 	Y \is_spread -> f \is_monotone_computable -> f \is_single_valued
 	-> x \is_computable_element -> f x y -> y \is_computable_element.
 Proof.
-move => sprd [M [mon comp]] sing [phi phinx] fxy.
-have phifd: phi \from_dom (eval M).
-	suffices phifd': (phi \from_dom (f o (delta (r:=X)))).
-		by have [y' [[Mphi [MphiMphi asd]] prop]]:= (comp phi phifd').1; exists Mphi.
-	exists y; split; first by exists x.
-	move => x' phinx'; exists y.
-	suffices: x = x' by move => <-.
-	by apply/ (rep_sing X); first by apply phinx.
-have Mop: (eval M) \is_computable_operator by exists M.
-have Msing: (eval M) \is_single_valued by apply/ mon_sing_op.
-have [N Nprop]:= (cmpt_op_cmpt phifd Mop Msing).
-have qfd: forall q, q \from_dom (fun (q' : questions Y) (a' : answers Y) =>
-  exists Ff : names Y, (eval M) phi Ff /\ Ff q' = a').
-	by move => q; have [Mphi MphiMphi]:= phifd; exists (Mphi q); exists Mphi.
-have Ntot: (meval N) \is_total by move => q; have [qfdN prop] := Nprop q (qfd q).
-apply/ (sprd y N).
-have [psi psiprop]:= choice (meval N) Ntot.
-have eq: forall Mphi, (eval M) phi Mphi -> Mphi = psi.
-	move => Mphi MphiMphi.
-	apply/ Msing; first by apply MphiMphi.
-	move => q'.
-	have [n eq]:= MphiMphi q'.
-	exists n;	rewrite eq; congr Some.
-	have Npsi: (meval N) q' (psi q') by apply psiprop.
-	have [Mpsi [MphiMpsi eq']]:= (Nprop q' (qfd q')).2 (psi q') Npsi.
-	suffices: Mphi = Mpsi by move => ->.
-	by apply/ Msing.
-exists psi.
-split.
-	move => q _.
-	split; first by exists (psi q); apply psiprop.
-	move => a evl.
-	have [Mphi [MphiMphi val]]:= (Nprop q (qfd q)).2 a evl.
-	by rewrite -(eq Mphi).
-have [Mphi MphiMphi] := phifd.
-rewrite -(eq Mphi) => //.
-have phiny: (f o (delta (r:=X))) phi y.
-	split; first by exists x.
-	move => x' phinx'.
-	exists y.
-	suffices: x' = x by move => ->.
-	by apply/ (rep_sing X); first by apply phinx'.
-have phifd': phi \from_dom (f o (delta (r:=X))) by exists y.
-have [[fx [[Mpsi [MphiMpsi Mpsinfx]]] prop'] prop]:= comp phi phifd'.
-have [fx' Mphinfx']:= prop' Mphi MphiMphi.
-rewrite -(Msing phi Mphi Mpsi) in Mpsinfx => //.
-have fdsing: (f o (rep X)) \is_single_valued.
-	apply/ comp_sing => //.
-	by apply (rep_sing X).
-suffices: fx = y by move => <-.
-apply/ fdsing; last by apply phiny.
-apply/prop.
-split; first by exists Mphi.
-move => Mphi' MphiMphi'.
-exists fx.
-by rewrite (Msing phi Mphi' Mphi).
+move => sprd mon sing [phi phinx] fxy.
+have xfd: x \from_dom f by exists y.
+have [M Mprop]:= sprd_cmpt_prec sprd mon.
+exists (M phi).
+have [y' [Mphiny' fxy']]:= (F2MF_rlzr f M).1 Mprop phi x phinx xfd.
+by rewrite (sing x y y').
 Qed.
+
+(*
+Lemma fun_sprd (X Y: rep_space) (someq: questions X): (X c-> Y) \is_spread.
+Proof.
+move => M.
+pose Macc := fix Macc n Lq := match Lq.1 with
+	| nil => (nil, M n Lq)
+	| cons p K => match M n (K, Lq.2) with
+		| Some a => ((cons p K), M n (K, Lq.2))
+		| None => (K, M n (K, Lq.2))
+	end
+end.
+pose psi := (fun Lq => match (Macc (length Lq.1) Lq).2 with
+	| Some a => a
+	| None => inl someq
+end).
+exists psi => phi f Mcomp phinf.
+rewrite /delta/=/is_fun_name/=.
+suffices <-: eval (U phi) =~= eval (U psi) by trivial.
+move => phi' Fphi'.
+split => ass q.
+	have [n val]:= ass q.
+	admit.
+have [n val] := ass q.
+exists n.
+rewrite /psi in val.
+Admitted.
+*)
+
 End SPREADS.
