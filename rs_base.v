2c2fce459aa30c4c00fc460511e0671b3652dbc9
Florian Steinberg <florian.steinberg@inria.fr>
2018-04-27 21:09:07 +0200

    added some documentation.

--- rs_base.v
+++ rs_base.v
@@ -36,10 +36,14 @@ Notation rep_sur X := (rep_valid X).2.
 Notation rep_sing X := (rep_valid X).1.
 
 Section REALIZERS.
+(* This is the abstract definition of what it means to be a realizer. The more usable definitions
+for special cases are frlzr and rrlzr and can be found below*)
+
 Definition rlzr (X Y: rep_space) (F: (names X) ->> (names Y)) (f: X ->> Y) :=
 	(rep Y) o F \tightens (f o (rep X)).
 Notation "f '\is_realized_by' F" := (rlzr F f) (at level 2).
 Notation "F '\is_realizer_of' f" := (rlzr F f) (at level 2).
+
 Global Instance rlzr_prpr (X Y: rep_space):
 	Proper (@equiv (names X) (names Y) ==> @equiv (space X) (space Y) ==> iff) (@rlzr X Y).
 Proof. by move => F G FeG f g feg; rewrite /rlzr FeG feg. Qed.
@@ -58,7 +62,7 @@ split => [rlzr phi [fx [[x [phinx eq]] prop]] | mfrlzr phi x phinx].
 		by exists (f x); rewrite -FphiFphi; exact: rlzr.
 	apply tot_comp; first exact: F2MF_tot; exists x.
 	split => //; rewrite -FphiFphi in Fphiny.
-	by apply: (representation_is_valid Y).1; [apply rlzr | apply/ Fphiny ].
+	by apply: (rep_sing Y); [apply rlzr | apply/ Fphiny ].
 have exte: ((delta (r:=Y)) o (F2MF F)) \extends ((F2MF f) o (delta (r:=X))).
 	apply/ exte_tight => //; apply: comp_sing; try exact: F2MF_sing.
 		exact: (rep_sing X).
@@ -77,20 +81,15 @@ have eq': (F2MF f) =~= (F2MF g) by move => s; rewrite /F2MF (feg s).
 by rewrite !frlzr_rlzr eq eq'.
 Qed.
 
-
-Lemma mfrlzr_rlzr (X Y: rep_space) F (f: X ->> Y) (somey: Y): f \is_single_valued -> f \is_total
-	-> (exists g, g \is_choice_for f /\ F \is_realizer_function_for g) <-> (F2MF F) \is_realizer_of f.
+Lemma mfrlzr_rlzr (X Y: rep_space) F (f: X ->> Y) (somey: Y): f \is_single_valued -> f \is_total ->
+	(exists g, g \is_choice_for f /\ F \is_realizer_function_for g) <-> (F2MF F) \is_realizer_of f.
 Proof.
-move => sing tot.
-split => [ [g [icf rlzr]] | mfrlzr].
-	move: ((@sing_tot_F2MF_icf X Y f g sing tot).1 icf) => eq.
-	suffices: (F2MF F) \is_realizer_of (F2MF g) by rewrite /rlzr -eq.
+split => [[g [icf rlzr]] | mfrlzr].
+	rewrite -((@sing_tot_F2MF_icf X Y f g H H0).1 icf).
 	exact/ frlzr_rlzr.
-have ass: f \is_single_valued /\ f \is_total by split.
-have [g eq]:= ((F2MF_sing_tot f (somey)).1 ass).
+have [g eq]:= ((F2MF_sing_tot f (somey)).1 (conj H H0)).
 exists g; split; first by apply sing_tot_F2MF_icf.
-apply/ frlzr_rlzr.
-by rewrite /rlzr eq.
+by apply/ frlzr_rlzr; rewrite eq.
 Qed.
 
 Lemma icf_rlzr (X Y: rep_space) F (f: X ->> Y):
@@ -98,28 +97,21 @@ Lemma icf_rlzr (X Y: rep_space) F (f: X ->> Y):
 Proof.
 move => rlzr G icf.
 apply/ tight_trans; last by apply rlzr.
-apply/ tight_comp_r.
-exact/ icf_F2MF_tight.
+exact/ tight_comp_r/icf_F2MF_tight.
 Qed.
 
 Lemma tight_rlzr (X Y: rep_space) G F (f: X ->> Y):
 	G \tightens F -> F \is_realizer_of f -> G \is_realizer_of f.
 Proof.
-move => GtF Frf.
-rewrite /rlzr.
-apply/ tight_trans.
-apply/ tight_comp_r; first by apply GtF.
-apply Frf.
+by move => GtF Frf; apply/ tight_trans; [apply/tight_comp_r/ GtF | apply Frf].
 Qed.
 
 Lemma rlzr_comp (X Y Z: rep_space) G F (f: X ->> Y) (g: Y ->> Z):
 	G \is_realizer_of g -> F \is_realizer_of f -> (G o F) \is_realizer_of (g o f).
 Proof.
 move => Grg Frf.
-rewrite /rlzr.
-rewrite -comp_assoc.
-apply/ tight_trans.
-	by apply /tight_comp_l; apply Grg.
+rewrite /rlzr -comp_assoc.
+apply/ tight_trans; first by apply /tight_comp_l; apply Grg.
 apply/ tight_trans.
 	rewrite comp_assoc.
 	by apply /tight_comp_r; apply Frf.
@@ -131,8 +123,7 @@ Lemma rlzr_dom (X Y: rep_space) (f: X ->> Y) F:
 Proof.
 move => rlzr phi x phinx [y fxy].
 have phifd: phi \from_dom (f o (delta (r:=X))).
-	exists y.
-	split; first by exists x.
+	exists y; split; first by exists x.
 	move => x' phinx'.
 	by rewrite (rep_sing X phi x' x); first by exists y.
 have [y' [[Fphi [FphiFphi Fphiny']]] _]:= (rlzr phi phifd).1.
@@ -161,8 +152,7 @@ Lemma sing_rlzr (X Y: rep_space) (f: X ->> Y) F: F \is_single_valued -> f \is_si
 	(forall phi x Fphi y, phi \is_name_of x -> f x y -> F phi Fphi -> Fphi \is_name_of y).
 Proof.
 move => Fsing fsing.
-split.
-	by move => Frf; split; [exact: rlzr_dom | exact: rlzr_val_sing].
+split; first by move => Frf; split; [exact: rlzr_dom | exact: rlzr_val_sing].
 move => [prp cnd] phi [y [[x [phinx fxy]] _]].
 have xfd: x \from_dom f by exists y.
 have [Fphi FphiFphi]:= prp phi x phinx xfd.
@@ -172,12 +162,10 @@ split.
 	move => Fphi' FphiFphi'; exists y.
 	by rewrite (Fsing phi Fphi' Fphi).
 move => y' [[Fphi' [FphiFphi' Fphi'ny']] _].
-split.
-	exists x; split => //.
-	rewrite (Fsing phi Fphi' Fphi) in Fphi'ny' => //.
-	by rewrite (rep_sing Y Fphi y' y).
-move => x' phinx'.
-by exists y; rewrite (rep_sing X phi x' x).
+split; last by exists y; rewrite (rep_sing X phi s x).
+exists x; split => //.
+rewrite (Fsing phi Fphi' Fphi) in Fphi'ny' => //.
+by rewrite (rep_sing Y Fphi y' y).
 Qed.
 
 Definition rrlzr (X Y: rep_space) (F: (names X) -> (names Y)) (f: X ->> Y) :=
@@ -186,31 +174,27 @@ Definition rrlzr (X Y: rep_space) (F: (names X) -> (names Y)) (f: X ->> Y) :=
 Notation "F '\is_rec_realizer_of' f":= (rrlzr F f) (at level 2).
 
 Lemma rrlzr_rlzr (X Y: rep_space) (f: X ->> Y) F:
-	F \is_rec_realizer_of f
-	<->
-	(F2MF F) \is_realizer_of f.
+	F \is_rec_realizer_of f <-> (F2MF F) \is_realizer_of f.
 Proof.
-split; last first.
-	move => Frf phi x phinx [y fxy].
-	have phifd: phi \from_dom (f o (delta (r:=X))).
-		exists y; split; first by exists x.
-		by move => x' phinx'; rewrite (rep_sing X phi x' x) => //; exists y.
-	have [[y' [[Fphi [<- Fphiny']] prp]] cnd]:= (Frf phi phifd).
-	have phiny': (delta (r:=Y)) o (F2MF F) phi y'.
-		split; first by exists (F phi).
-		by move => Fphi' <-; exists y'.
-	have [[x' [phinx' fx'y']]prp']:= cnd y' phiny'.
-	exists y'; split => //.
-	by rewrite (rep_sing X phi x x').
-move => prop phi [y [[x [phinx fxy]] prp]].
-have xfd: x \from_dom f by exists y.
-have [y' [Fphiny' fxy']]:= prop phi x phinx xfd.
 split.
-	exists y'; split; first by exists (F phi).
-	move => Fphi <-; by exists y'.
-move => y'' [[Fphi [<- Fphiny'']]cnd].
-split; first by exists x; rewrite (rep_sing Y (F phi) y'' y').
-by move => x' phinx'; rewrite (rep_sing X phi x' x).
+	move => prop phi [y [[x [phinx fxy]] prp]].
+	have xfd: x \from_dom f by exists y.
+	have [y' [Fphiny' fxy']]:= prop phi x phinx xfd.
+	split.
+		exists y'; split; first by exists (F phi).
+		by move => Fphi <-; exists y'.
+	move => y'' [[Fphi [<- Fphiny'']]cnd].
+	split; first by exists x; rewrite (rep_sing Y (F phi) y'' y').
+	by move => x' phinx'; rewrite (rep_sing X phi x' x).
+move => Frf phi x phinx [y fxy].
+have phifd: phi \from_dom (f o (delta (r:=X))).
+	exists y; split; first by exists x.
+	by move => x' phinx'; rewrite (rep_sing X phi x' x) => //; exists y.
+have [[y' [[Fphi [<- Fphiny']] prp]] cnd]:= (Frf phi phifd).
+have phiny': (delta (r:=Y)) o (F2MF F) phi y'.
+	by split; [exists (F phi) | move => Fphi' <-; exists y'].
+have [[x' [phinx' fx'y']]prp']:= cnd y' phiny'.
+by exists y'; split => //; rewrite (rep_sing X phi x x').
 Qed.
 
 Global Instance rrlzr_prpr (X Y: rep_space):
@@ -227,20 +211,17 @@ Lemma rlzr_F2MF (X Y: rep_space) (f: X -> Y) F:
 	forall phi x, phi \is_name_of x -> phi \from_dom F /\ forall Fphi, F phi Fphi -> Fphi \is_name_of (f x).
 Proof.
 split.
-	move => Frf phi x phinx; split; first by apply/ rlzr_dom; [apply Frf | apply phinx | apply F2MF_tot ].
-	by move => Fphi FphiFphi; apply/ rlzr_val_sing; [apply F2MF_sing | apply Frf | apply phinx | | ].
+	split; first by apply/ rlzr_dom; [apply H | apply H0 | apply F2MF_tot ].
+	by intros; apply/ rlzr_val_sing; [apply F2MF_sing | apply H | apply H0 | | ].
 move => prp phi [_ [[x [phinx _]] _]].
 split.
 	exists (f x).
 	split; last by move => Fphi FphiFphi; exists (f x); apply (prp phi x phinx).2.
 	have [Fphi FphiFphi]:= (prp phi x phinx).1.
-	exists Fphi; split => //.
-	by apply (prp phi x phinx).2.
+	by exists Fphi; split => //; apply (prp phi x phinx).2.
 move => y [[Fphi [FphiFphi Fphiny]] cnd].
-split; last by move => a b; exact: F2MF_tot.
-exists x; split => //.
-have Fphinfx := (prp phi x phinx).2 Fphi FphiFphi.
-by rewrite (rep_sing Y Fphi y (f x)).
+split; last by intros; exact: F2MF_tot.
+by exists x; split; last by rewrite (rep_sing Y Fphi y (f x)); last by apply (prp phi x phinx).2.
 Qed.
 
 Lemma frlzr_is_rep X Y:
@@ -249,11 +230,9 @@ Proof.
 split => [F f g Frf Frg | f].
 	apply functional_extensionality => x.
 	have [phi phinx]:= (rep_sur X x).
-	apply/ (rep_sing Y); first exact: (Frf phi x phinx).
-	exact: (Frg phi x phinx).
+	by apply/ (rep_sing Y); [apply: (Frf phi x) | apply: (Frg phi x)].
 set R :=(fun phi psi => phi \from_dom (rep X) -> forall x, (rep X) phi x -> (rep Y) psi (f x)).
-have Rtot: R \is_total.
-	move => phi.
+have Rtot: R \is_total => [phi | ].
 	case: (classic (phi \from_dom (rep X))) => [[x phinx] | nfd].
 		have [psi psiny]:= (rep_sur Y (f x)).
 		by exists psi => _ x' phinx'; rewrite -(rep_sing X phi x x').
