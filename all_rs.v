3e2ad88e75f18888326a1d73f66445b42e77a80b
FlorianSteinberg <florian.steinberg@inria.fr>
2018-05-15 12:11:09 +0200

    removed rs library files.

--- all_rs.v
+++
@@ -1,9 +0,0 @@
-Require Export all_rs_base.
-Require Export rs_one.
-Require Export rs_nat.
-Require Export rs_opt.
-Require Export rs_usig.
-Require Export rs_list.
-Require Export rs_dscrt.
-Require Export rs_sets.
-Require Export rs_sprd.
